6be48e1b77d8d62e7fecf11661261244853b9ebd
Arjunkuls <63905318+Arjunkuls@users.noreply.github.com>
2020-12-07 11:45:28 +0530

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,82 @@
+class Snake {
+  constructor() {
+    this.body = [];
+    this.body[0] = createVector(floor(w / 2), floor(h / 2));
+    this.xdir = 0;
+    this.ydir = 0;
+    this.len = 0;
+    this.isAdmin = false;
+  }
+
+  setDir(x, y) {
+    this.xdir = x;
+    this.ydir = y;
+  }
+
+  update() {
+    let head = this.body[this.body.length - 1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+
+  grow() {
+    let head = this.body[this.body.length - 1].copy();
+    this.len++;
+    this.body.push(head);
+  }
+
+  endGame() {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    // if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+    //   return true;
+    // }
+    if (x > w - 1 ){
+      this.body[this.body.length - 1].x = 0 //+ this.body.length
+    }
+    if (x <= 0){
+      this.body[this.body.length - 1].x = w //- this.body.length
+    }
+    if (y > h - 1){
+      this.body[this.body.length - 1].y = 0 //+ this.body.length
+    }
+    if(y<0){
+      this.body[this.body.length - 1].y = h //+ this.body.length
+    }
+
+
+
+    if (this.isAdmin === false){
+    for (let i = 0; i < this.body.length - 1; i++) {
+      let part = this.body[i];
+      if (part.x == x && part.y == y) {
+        return true;
+      }
+    }}
+    return false;
+  }
+
+  eat(pos) {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x == pos.x && y == pos.y) {
+      this.grow();
+      return true;
+    }
+    return false;
+  }
+
+  show() {
+    for (let i = 0; i < this.body.length; i++) {
+      fill(255);
+      noStroke();
+      rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
+    if (keyIsDown(65)&&keyIsDown(75)){
+      this.isAdmin = true;
+      this.grow();
+    }
+  }
+}
